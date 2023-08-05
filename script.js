6ffa21b48b9684aaf6380689f5ae06a7ea983947
unknown <kudachipangula@gmail.com>
2023-08-05 03:43:02 +0200

    Create subtraction function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Create the birth year and current year as input.
+let birthYear = parseInt(prompt("Enter birth Year: "));
+let currentYear = parseInt(prompt("Enter current year: "));
+
 //Create a subtraction function
+function subtraction() {}
 //Calculate the age by subtracting the birth year from the current year.
 //Invoke subtraction function to run calculated age.

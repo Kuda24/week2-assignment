bd38c99732a5fe565cd42efa6ccec9b5cfe856eb
unknown <kudachipangula@gmail.com>
2023-08-05 03:53:13 +0200

    Add age calculator and array loop logic

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function subtraction() {
 
 //Invoke subtraction function to run calculated age.
 console.log(subtraction());
+
+console.log(subtraction());
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log("Even numbers in the array:");
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] % 2 === 0) {
+    console.log(numbers[i]);
+  }
+}

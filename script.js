49c259e1fe5b2cab2cdd6d816b25512a232f20d4
unknown <kudachipangula@gmail.com>
2023-08-05 03:35:42 +0200

    Create pseudocode for the age calculation

---
+++ script.js
@@ -0,0 +1,4 @@
+//Create the birth year and current year as input.
+//Create a subtraction function
+//Calculate the age by subtracting the birth year from the current year.
+//Invoke subtraction function to run calculated age.
